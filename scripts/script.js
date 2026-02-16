3c1afb4e1179e0ba8d768a8c695750075cc9b4c0
Neo <neogoldahlgren@gmail.com>
2026-02-16 16:59:01 +0100

    Featured projects showing through js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,25 @@
 const current = document.querySelector("#current");
 const featured = document.querySelectorAll(".home-project");
+const featuredProjects = [
+    {
+        title: "SnGeo - Geography Game", 
+        desc: "Geography trivia game using an external geography api. Currently unfinished, but population and curreny games work!",
+        languages: ["HTML", "CSS", "JS"],
+        link: "#"
+    },
+    {
+        title: "2025-26 NFL Season",
+        desc: "A website containing information about the 2025-26 NFL Season. Includes information about games, statistics, standings and the full 2025 NFL Draft.",
+        languages: ["HTML", "CSS", "JS", "PHP"],
+        link: "#"
+    },
+    {
+        title: "Coming Soon...",
+        desc: "Be patient and make sure to check back later! :)",
+        languages: ["TBD", "TBD", "TBD"],
+        link: "#"
+    }
+]
 const navLinks = document.querySelectorAll("header nav a");
 
 navLinks.forEach((x) => {
@@ -28,7 +48,37 @@ function randomGreenishColor(element, color = null) {
     }
 };
 
+function showFeaturedProjects() {
+    for (i=0; i<featured.length; i++) {
+        featured[i].innerHTML = `
+        <h3>${featuredProjects[i].title}</h3>
+        <p>${featuredProjects[i].desc}</p>
+        <div class="project-languages"></div>
+        `
+    }
+
+    let projectLangDiv = document.querySelectorAll(".project-languages");
+    for (i=0; i<projectLangDiv.length; i++) {
+        for (j=0; j<=featuredProjects[i].languages.length; j++) {
+            if (j === featuredProjects[i].languages.length) {
+                let div = document.createElement("div");
+                div.classList.add("project-link");
+                div.innerHTML = `<a href="${featuredProjects[i].link}">Visit Project →</a>`;
+                projectLangDiv[i].appendChild(div);
+                break;
+            }
+
+            let div = document.createElement("div");
+            div.classList.add("language");
+            div.classList.add(featuredProjects[i].languages[j].toLowerCase());
+            div.textContent = featuredProjects[i].languages[j];
+            projectLangDiv[i].appendChild(div);
+        }
+    }
+}
+
 randomGreenishColor(current);
 featured.forEach((x) => {
     randomGreenishColor(x);
 });
+showFeaturedProjects();
